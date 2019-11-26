d0b6d5ac775399f484d5cfc1d1c089bdcadd53b1
cerwyn <cerwyneliata.c@gmail.com>
2019-11-26 14:01:04 +0700

    Add Flutter Web App

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(
     })
   );
 
+  app.use('/web', express.static('web'));
+
   mongoose
   .connect(
     `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin`
